9e609bc5c1751e60278175297c999dae828f3f41
Saadman Sakib <saadman.sakib.ss@gmail.com>
2022-09-12 13:54:18 +0600

    :technologist: chore: README.md add

---
+++ src/data.js
@@ -0,0 +1,68 @@
+export default [
+    {
+        title: "Dhaka University",
+        location: "Dhaka",
+        maps: "dhaka",
+        date: {
+            start: "",
+            end: ""
+        },
+        description: "",
+        img: ""
+    },
+    {
+        title: "Dhaka University",
+        location: "Dhaka",
+        maps: "dhaka",
+        date: {
+            start: "",
+            end: ""
+        },
+        description: "",
+        img: ""
+    },
+    {
+        title: "Dhaka University",
+        location: "Dhaka",
+        maps: "dhaka",
+        date: {
+            start: "",
+            end: ""
+        },
+        description: "",
+        img: ""
+    },
+    {
+        title: "Dhaka University",
+        location: "Dhaka",
+        maps: "dhaka",
+        date: {
+            start: "",
+            end: ""
+        },
+        description: "",
+        img: ""
+    },
+    {
+        title: "Dhaka University",
+        location: "Dhaka",
+        maps: "dhaka",
+        date: {
+            start: "",
+            end: ""
+        },
+        description: "",
+        img: ""
+    },
+    {
+        title: "Dhaka University",
+        location: "Dhaka",
+        maps: "dhaka",
+        date: {
+            start: "",
+            end: ""
+        },
+        description: "",
+        img: ""
+    }
+]
